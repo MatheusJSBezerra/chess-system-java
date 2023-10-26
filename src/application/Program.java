5303cef88ad6067c71db117f65fec819f31a8af6
matheusjsbezerra <matheusjeroan@gmail.com>
2023-10-26 18:59:57 -0300

    Aula 189

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     ChessMatch chessMath = new ChessMatch();
+    while(true){
+      UI.printBoard(chessMath.getPieces());
+      System.out.println();
+      System.out.println("Source: ");
+      ChessPosition source = UI.readChessPosition(sc);
 
-    UI.printBoard(chessMath.getPieces());
+      System.out.println();
+      System.out.println("Target: ");
+      ChessPosition target = UI.readChessPosition(sc);
+
+      ChessPiece capturedPiece = chessMath.performChessMove(source, target);c2
+    }
   }
 }
