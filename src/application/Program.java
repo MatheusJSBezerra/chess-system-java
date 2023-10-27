cdcb7d091c803c794d11495556e7fa9547fa8e79
matheusjsbezerra <matheusjeroan@gmail.com>
2023-10-27 18:44:44 -0300

    Aula 199

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
     Scanner sc = new Scanner(System.in);
     ChessMatch chessMath = new ChessMatch();
     List<ChessPiece> captured = new ArrayList<>();
-    while(true){
+    while(!chessMath.getCheckMate()){
       try{
         UI.clearScreen();
         UI.printMatch(chessMath, captured);
@@ -46,5 +46,8 @@ public class Program {
         sc.nextLine();
       }
     }
+
+    UI.clearScreen();
+    UI.printMatch(chessMath, captured);
   }
 }
